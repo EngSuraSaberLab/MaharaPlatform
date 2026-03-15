063d660465345cc714f00a5ff42248b0636a2358
Sura Saber <YOUR_GITHUB_EMAIL@example.com>
2026-03-15 10:54:03 +0300

    Add language-prefixed URLs

--- project/project/urls.py
+++ project/project/urls.py
@@ -9,21 +9,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from courses.views import media_file
 
 
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
 
-    path("admin/", admin.site.urls),
+    path("media/<path:file_path>", media_file),
+]
 
+urlpatterns += i18n_patterns(
+    path("admin/", admin.site.urls),
     path("accounts/", include("registration.urls")),
     path("blog/", include("blog.urls")),
     path("payments/", include("payments.urls")),
     path("", include("courses.urls")),
-
-    path("media/<path:file_path>", media_file),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
